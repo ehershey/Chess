ec9a5fd31e0c0742c1a58ede2edeff1b22c06a0a
Michaelangel007 <testcraft@sharedcraft.com>
2015-08-31 22:18:40 -0700

    Add Queen Bishop and Knight testing

--- chess.cpp
+++ chess.cpp
@@ -10,6 +10,7 @@ int main()
 {
     bitboard_t board;
 
+#if 0
     board = BitBoardMakeWhiteSquares();
     printf( "White Squares\n" );
     BitBoardPrint( board );
@@ -19,12 +20,105 @@ int main()
     printf( "Black Squares\n" );
     BitBoardPrint( board );
     printf( "\n" );
+#endif
 
+#if 0
     State_t state;
     state.Init();
     state.Dump();
 
     state.PrettyPrintBoard();
+#endif
+
+#if 0
+    for( int cell = 0; cell < 64; cell++ )
+    {
+        //int col      = cell & 7;
+        //int row      = cell / 8;
+        //int rankfile = (row << 4) + col;
+
+        int col, row, rankfile;
+        CellToColRow( cell, col, row );
+        rankfile = ColRowToRankFile( col, row );
+
+        printf( "[%d,%d]: 0x%02X %c%c -> %d\n", col, row, rankfile, aFILE[ col ], aRANK[ row ], (row + col) );
+    }
+#endif
+
+
+#if 0
+    printf( "= Queen Moves =\n" );
+
+        board = BitBoardMovesColorQueen( 0x00 );
+        BitBoardPrint( board );
+        printf( "\n" );
+
+        board = BitBoardMovesColorQueen( 0x07 );
+        BitBoardPrint( board );
+        printf( "\n" );
+
+        board = BitBoardMovesColorQueen( 0x70 );
+        BitBoardPrint( board );
+        printf( "\n" );
+
+        board = BitBoardMovesColorQueen( 0x77 );
+        BitBoardPrint( board );
+        printf( "\n" );
+
+        for( int cell = 0; cell < 64; cell++ )
+        {
+            int col, row, rankfile;
+            CellToColRow( cell, col, row );
+            rankfile = ColRowToRankFile( col, row );
+
+            printf( "[%d,%d]: 0x%02X %c%c\n", col, row, rankfile, aFILE[ col ], aRANK[ row ] );
+            board = BitBoardMovesColorQueen( rankfile );
+            BitBoardPrint( board );
+            printf( "\n" );
+        }
+#endif
+
+
+#if 1
+    printf( "= Knight Moves =\n" );
+
+        //board = BitBoardMovesColorKnight( 0x33 ); // D4
+        board = BitBoardMovesColorKnight( 0x30 ); // A4
+        BitBoardPrint( board );
+        printf( "\n" );
+if(0)
+        for( int cell = 0; cell < 64; cell++ )
+        {
+            int col, row, rankfile;
+            CellToColRow( cell, col, row );
+            rankfile = ColRowToRankFile( col, row );
+
+            printf( "[%d,%d]: 0x%02X %c%c\n", col, row, rankfile, aFILE[ col ], aRANK[ row ] );
+            board = BitBoardMovesColorKnight( rankfile );
+            BitBoardPrint( board );
+            printf( "\n" );
+        }
+#endif
+
+#if 0
+    printf( "= Bishop Moves =\n" );
+
+        BitBoardPrint( board );
+        printf( "\n" );
+
+        for( int cell = 0; cell < 64; cell++ )
+        {
+            int col, row, rankfile;
+            CellToColRow( cell, col, row );
+            rankfile = ColRowToRankFile( col, row );
+
+            printf( "[%d,%d]: 0x%02X %c%c\n", col, row, rankfile, aFILE[ col ], aRANK[ row ] );
+            board = BitBoardMovesColorBishop( rankfile );
+            BitBoardPrint( board );
+            printf( "\n" );
+        }
+#endif
+
 
     return 0;
 }
