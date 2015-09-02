15a99970998078d034973f7cb19a0d8e761f214a
michaelangel007 <testcraft@sharedcraft.com>
2015-09-02 12:01:36 -0700

    Add BitBoardToRankFile()

--- bitboard.h
+++ bitboard.h
@@ -157,6 +157,7 @@ bitboard_t BitBoardMakeRank( uint8_t row )
 /** @param rankfile bit-packed rank and file
     00 = a1
     77 = h8
+   @see BitBoardMakeLocation() BitBoardToRankFile()
 */
 bitboard_t BitBoardMakeLocation( uint8_t rankfile )
 {
@@ -165,6 +166,39 @@ bitboard_t BitBoardMakeLocation( uint8_t rankfile )
 
     bitboard_t board = ((bitboard_t) aColMask[ col ]) << (8 * row);
     return board;
+}
+
+
+/** @return rankfile
+   @see BitBoardMakeLocation() BitBoardToRankFile()
+*/
+uint8_t BitBoardToRankFile( const bitboard_t board )
+{
+    // Optimization: Find First Set Bit Position (63=MSB, 0=LSB)
+    // C++: __builtin_clz
+    // Asm: x86: BSR    Bit Scan Reverse
+    //      PPC: cntlz  Count Leading Zeroes
+
+    // http://stackoverflow.com/questions/671815/what-is-the-fastest-most-efficient-way-to-find-the-highest-set-bit-msb-in-an-i
+    // http://stackoverflow.com/questions/355967/how-to-use-msvc-intrinsics-to-get-the-equivalent-of-this-gcc-code
+
+    if( 0 == board )
+        return 0xFF; // Invalid !
+
+    bitboard_t search = board;
+    uint8_t nRankFile = 0;
+
+    while( !(search & 0xFF) ) // nRowMask = 0xFF
+    {
+        search >>= 8;
+        nRankFile += 0x10; // rank
+    }
+
+    for( int x = 0; x < 8; x++ )
+        if( search & aColMask[ x ] )
+            return (nRankFile + x);
+
+    return 0xFF; // WTF?! Invalid!
 }
 
 
