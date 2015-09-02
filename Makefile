3a04cddbb27222a4d0e567108bd8c71822059649
Michaelangel007 <testcraft@sharedcraft.com>
2015-09-02 13:03:12 -0700

    Add OpenMP support

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
+# OSX g++
+CC=/usr/local/bin/g++
+CFLAGS=-Wall -Wextra
+LIB_OMP=-fopenmp
+LIBS=$(LIB_OMP)
+
+
 all: chess
 
 includes = $(wildcard *.h)
 
 chess: chess.cpp ${includes}
-	g++ $< -o $@ -Wall -Wextra
+	$(CC) $(CFLAGS) $< -o $@ $(LIBS)
 
--- chess.cpp
+++ chess.cpp
@@ -2,17 +2,61 @@
 #include <stdint.h> // uint64_t
 #include <string.h> // memcpy
 
+// BEGIN OMP
+    #include <omp.h>
+// END OMP
+
 #include "utility.h"
 #include "bitboard.h"
 #include "pieces.h"
 #include "state.h"
 #include "game.h"
 
-int giThreads = 0;
-int gnThreads = 0;
+// BEGIN OMP
+int gnThreadsMaximum = 0 ;
+int gnThreadsActive  = 0 ; // 0 = auto detect; > 0 manual # of threads
 
-int main()
+void InitMulticore()
 {
+// BEGIN OMP
+    gnThreadsMaximum = omp_get_num_procs();
+
+    if(!gnThreadsActive) // user didn't specify how many threads to use, default to all of them
+        gnThreadsActive = gnThreadsMaximum;
+    else
+        omp_set_num_threads( gnThreadsActive );
+
+    printf( "Using: %d / %d cores\n", gnThreadsActive, gnThreadsMaximum );
+
+    for( int iCore = 0; iCore < gnThreadsActive; iCore++ )
+    {
+    }
+}
+// END OMP
+
+int main( const int nArg, const char *aArg[] )
+{
+    (void) nArg;
+    (void) aArg;
+
+    InitMulticore();
+
+#if 1
+        printf( "= Queen - Print each potential move =\n" );
+
+        uint8_t nRankFile = 0x24;
+        bitboard_t board = BitBoardMovesColorQueen( nRankFile );
+
+        uint8_t nMoves;
+        uint8_t aMoves[ 32 ];
+        BitBoardToRankFileAllMoves( board, nMoves, aMoves );
+        printf( "Moves: %d\n", nMoves );
+        for( int iMove = 0; iMove < nMoves; iMove++ )
+        {
+            printf( "[%2d]: 0x%02X\n", iMove, aMoves[ iMove ] );
+        }
+#endif
+
     // spin-lock
     // wait for command    
 
