a58248b3c7e4c670b7688a98e36273c11be63a15
Michaelangel007 <testcraft@sharedcraft.com>
2015-09-02 18:51:09 -0700

    Fixup flags for Castling

--- state.h
+++ state.h
@@ -102,6 +102,16 @@ struct StateBitBoard_t
         return board;
     }
 
+    uint8_t GetPiece( const uint8_t rankfile )
+    {
+        bitboard_t board = BitBoardMakeLocation( rankfile );
+
+        for( int iPiece = PIECE_PAWN; iPiece < NUM_PIECES; iPiece++ )
+            if( _aBoards[ iPiece ] & board )
+                return iPiece;
+
+        return PIECE_EMPTY;
+    }
 };
 
 enum StateFlags_e
@@ -109,34 +119,37 @@ enum StateFlags_e
      STATE_CHECK             = (1 << 0)
     ,STATE_CHECKMATE         = (1 << 1)
     ,STATE_STALEMATE         = (1 << 2)
-    ,STATE_CAN_CASTLE_K_SIDE = (1 << 3)
+    ,STATE_CAPTURE_ENEMY     = (1 << 3)
     ,STATE_CAN_CASTLE_Q_SIDE = (1 << 4)
+    ,STATE_CAN_CASTLE_K_SIDE = (1 << 5)
+    ,STATE_CASTLED_Q_SIDE    = (1 << 6)
+    ,STATE_CASTLED_K_SIDE    = (1 << 7)
   //,STATE_HAS_CASTLED       = (1 << 3) // CanCastle( _flags & (STATE_CAN_CASTLE_K_SIDE|STATE_CAN_CASTLE_Q_SIDE) )
-    ,STATE_GAME_MID          = (1 << 5) // 1=mid game, 0=early or end-game
+  //,STATE_GAME_MID          = (1 << 8) // 1=mid game, 0=early or end-game
 };
 
 struct State_t
 {
     StateBitBoard_t _player[ NUM_PLAYERS ]; // 96 bytes if not storing board[ PIECE_EMPTY ]
     //                            //      96  112 bytes
-    float           _nEval      ; // +4  100  116
-    uint16_t        _nTurn      ; // +2  102  118  Even=White, Odd=Black
-    uint16_t        _iParent    ; // +2  104  120  i-node of parent
-    uint16_t        _iFirstChild; // +2  106  122  i-node of first child
-    uint8_t         _bFlags     ; // +1  107  123
-    uint8_t         _iFrom      ; // +1  108  124  RankFile (0xROWCOL)
-    uint8_t         _iTo        ; // +1  109  125  RankFile (0xROWCOL)
-    uint8_t         _ePiece     ; // +1  110  126  which piece was moved
-    uint8_t         _bPawnsMoved; // +1  111  127  Bitflags if pawn[col] has moved
-    uint8_t         _nChildren  ; // +1  112  128  total children
-//  uint8_t         _iGamePhase ; // +1  113  129  0=early game, 0x10=mid-game, 0x20=end-game
+    int16_t         _nEval      ; // +2   98  114
+    uint16_t        _nTurn      ; // +2  100  116  Even=White, Odd=Black
+    uint16_t        _iParent    ; // +2  102  118  i-node of parent
+    uint16_t        _iFirstChild; // +2  104  120  i-node of first child
+    uint16_t        _bFlags     ; // +2  106  122
+    uint8_t         _iFrom      ; // +1  107  123  RankFile (0xROWCOL)
+    uint8_t         _iTo        ; // +1  108  124  RankFile (0xROWCOL)
+    uint8_t         _ePiece     ; // +1  109  125  which piece was moved
+    uint8_t         _bPawnsMoved; // +1  110  126  Bitflags if pawn[col] has moved
+    uint8_t         _nChildren  ; // +1  111  127  total children
+//  uint8_t         _iGamePhase ; // +1  112  128  0=early game, 0x10=mid-game, 0x20=end-game
 
     void Init()
     {
 printf( "INFO: Boards[]: %u bytes\n", (uint32_t) sizeof( _player ) );
 printf( "INFO: State   : %u bytes\n", (uint32_t) sizeof( *this    ) );
 
-        _nEval  = 0.f;
+        _nEval  = 0;
         _bFlags = 0;
         _nTurn  = 0;
         _iFrom  = 0;
@@ -193,15 +206,23 @@ printf( "INFO: State   : %u bytes\n", (uint32_t) sizeof( *this    ) );
         board.Print( bPrintRankFile );
     }
 
+inline uint8_t GetColorPlayer() { return  _nTurn & 1; }
+inline uint8_t GetColorEnemy () { return ~_nTurn & 1; }
+
+    bitboard_t GetAllPieces( int iPlayer )
+    {
+        return _player[ iPlayer ].GetBoardAllPieces();
+    }
+
     bool IsCheck()
     {
-        uint8_t iPlayer = _nTurn  & 1;
-        uint8_t iEnemy  = iPlayer ^ 1;
+        uint8_t iPlayer = GetColorPlayer();
+        uint8_t iEnemy  = GetColorEnemy ();
 
-        StateBitBoard_t *pStateUs   = &_player[ iPlayer ];
-        StateBitBoard_t *pStateThem = &_player[ iEnemy  ];
+//        StateBitBoard_t *pStateUs   = &_player[ iPlayer ];
+//        StateBitBoard_t *pStateThem = &_player[ iEnemy  ];
 
-        bitboard_t origin           = pStateUs->_aBoards[ PIECE_KING ];
+        bitboard_t origin           = _player[ iPlayer ]._aBoards[ PIECE_KING ];
         uint8_t    kingRankFile     = BitBoardToRankFile( origin );
 
         // From the King's location
@@ -213,7 +234,6 @@ printf( "INFO: State   : %u bytes\n", (uint32_t) sizeof( *this    ) );
             bitboard_t movesPotential = 0;
             bitboard_t movesValid     = 0;
 
-(void) pStateThem;
 (void) movesAll;
 (void) movesPotential;
 (void) movesValid;
@@ -240,26 +260,163 @@ printf( "INFO: State   : %u bytes\n", (uint32_t) sizeof( *this    ) );
 
     bool IsValidMove( uint8_t fromRankFile, uint8_t toRankFile )
     {
-        int iPiece = 0;
         bitboard_t origin = BitBoardMakeLocation( fromRankFile );
         bitboard_t dest   = BitBoardMakeLocation( toRankFile );
 
         // get the piece type
-        for( int iPiece = PIECE_QUEEN; iPiece > PIECE_PAWN; iPiece-- )
-        {
-        }
+        uint8_t iPlayer    = GetColorPlayer();
+        uint8_t iEnemy     = GetColorEnemy ();
 
+        uint8_t iPieceSrc  = _player[ iPlayer ].GetPiece( fromRankFile );
+        uint8_t iPieceDst  = _player[ iPlayer ].GetPiece( toRankFile );
+        uint8_t iEnemyDst  = _player[ iEnemy  ].GetPiece( toRankFile ); // check for capture
+
+        if( iPieceSrc == PIECE_EMPTY )
+            return false;
+
+(void) iPieceDst;
+(void) iEnemyDst;
 (void) origin;
 (void) dest;
-(void) iPiece;
 
+        // If dest is same color as player mark invalid
+
+        // If new state IsCheck() not a valid move
+
+        // If dest is enemy color
+
+        // Except if King (this color) captures Rook (this color)
+        // And King hasn't moved
+        // And Rook hasn't moved
+
+        // Check Line-of-Sight from piece
 
         return false; // FIXME:
     }
 
+    void Move( uint8_t fromRankFile, uint8_t toRankFile )
+    {
+        uint8_t iPlayer    = GetColorPlayer();
+        uint8_t iPieceSrc  = _player[ iPlayer ].GetPiece( fromRankFile );
+
+(void) iPlayer;
+
+        // Get which player the destination is
+
+        int bCanCastleQ = _bFlags & STATE_CAN_CASTLE_Q_SIDE;
+        int bCanCastleK = _bFlags & STATE_CAN_CASTLE_K_SIDE;
+        int bCanCastle  = bCanCastleK | bCanCastleQ;
+
+        if( iPieceSrc == PIECE_ROOK )
+        {
+            if( iPlayer == PLAYER_WHITE )
+            {
+                if( bCanCastleQ && (fromRankFile == 0x00) )
+                    _bFlags &= ~STATE_CAN_CASTLE_Q_SIDE; // mark can't castle
+
+                if( bCanCastleK && (fromRankFile == 0x07) )
+                    _bFlags &= ~STATE_CAN_CASTLE_K_SIDE; // mark can't castle
+            }
+            else // PLAYER_BLACK
+            {
+                if( bCanCastleQ && (fromRankFile == 0x70) )
+                    _bFlags &= ~STATE_CAN_CASTLE_Q_SIDE; // mark can't castle
+
+                if( bCanCastleK && (fromRankFile == 0x77) )
+                    _bFlags &= ~STATE_CAN_CASTLE_K_SIDE; // mark can't castle
+            }
+        }
+        else
+        if( iPieceSrc == PIECE_KING )
+        {
+
+            if( bCanCastle )
+            {
+                bool bJustCastled = false;
+                if( iPlayer == PLAYER_WHITE )
+                {
+                    if( bCanCastleQ && (fromRankFile == 0x04) && (toRankFile == 0x00) )
+                    {
+                        // Place King 0x02 C1 -- IsCheck()?
+                        // Place King 0x03 D1 -- IsCheck()?
+                        _bFlags &= ~bCanCastle    ;
+                        _bFlags |=  STATE_CASTLED_Q_SIDE;
+                        bJustCastled = true;
+                    }
+
+                    if( bCanCastleK && (fromRankFile == 0x05) && (toRankFile == 0x07) )
+                    {
+                        // Place King 0x05 F1 -- IsCheck()?
+                        // Place King 0x06 G1 -- IsCheck()?
+                        _bFlags &= ~bCanCastle    ;
+                        _bFlags |=  STATE_CASTLED_K_SIDE;
+                        bJustCastled = true;
+                    }
+                }
+
+                if( iPlayer == PLAYER_BLACK )
+                {
+                    if( bCanCastleQ && (fromRankFile == 0x74) && (toRankFile == 0x70) )
+                    {
+                        // Place King 0x72 C8 -- IsCheck()?
+                        // Place King 0x73 D8 -- IsCheck()?
+                        _bFlags &= ~bCanCastle    ;
+                        _bFlags |=  STATE_CASTLED_Q_SIDE;
+                        bJustCastled = true;
+                    }
+
+                    if( bCanCastleK && (fromRankFile == 0x75) && (toRankFile == 0x77) )
+                    {
+                        // Place King 0x75 F8 -- IsCheck()?
+                        // Place King 0x76 G8 -- IsCheck()?
+                        _bFlags &= ~bCanCastle    ;
+                        _bFlags |=  STATE_CASTLED_K_SIDE;
+                        bJustCastled = true;
+                    }
+                }
+
+                if( bJustCastled )
+                {
+                    // _bFlags |=  STATE_GAME_MID;
+
+                    bitboard_t oldKing = BitBoardMakeLocation( fromRankFile );
+                    bitboard_t oldRook = BitBoardMakeLocation( toRankFile   );
+
+                    // Remove old king, old rook
+                    _player[ iPlayer ]._aBoards[ PIECE_KING ] &= ~oldKing;
+                    _player[ iPlayer ]._aBoards[ PIECE_ROOK ] &= ~oldRook;
+
+                    bitboard_t newKing = 0;
+                    bitboard_t newRook = 0;
+
+                    if( _bFlags & STATE_CASTLED_K_SIDE )
+                    {
+                        newKing = BitBoardMakeLocation( 0x06 + 0x70*iPlayer );
+                        newRook = BitBoardMakeLocation( 0x05 + 0x70*iPlayer );
+                    }
+                    else
+                    {
+                        newKing = BitBoardMakeLocation( 0x20 + 0x70*iPlayer );
+                        newRook = BitBoardMakeLocation( 0x50 + 0x70*iPlayer );
+                    }
+
+                    // Place new king, new rook
+                    _player[ iPlayer ]._aBoards[ PIECE_KING ] |= newKing;
+                    _player[ iPlayer ]._aBoards[ PIECE_ROOK ] |= newRook;
+                }
+            }
+        }
+    }
+
+    void Capture( uint8_t fromRankFile, uint8_t toRankFile )
+    {
+(void) fromRankFile;
+(void) toRankFile ;
+    }
+
     float Eval()
     {
-        _nEval = 0.f;
+        _nEval = 0;
 
         return _nEval;
     }
