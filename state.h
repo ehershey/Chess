d5daee0f76ee97e8414806e46523abac95eb6f39
michaelangel007 <testcraft@sharedcraft.com>
2015-09-04 09:31:49 -0700

    Change PrettyPrintBoard to RawBoard, add PrintCompactBoard(), refactor BitBoardToRaw

--- state.h
+++ state.h
@@ -7,7 +7,7 @@
     // Version 3: int32[8]    //  32 bytes = 1 nibble/cell
     const int  CELLS_SIZE = 64;
 
-struct PrettyPrintBoard_t
+struct Board_t
 {
     char _cells[ CELLS_SIZE ]; // Version 2: 1 byte/cell
     //uint32_t _cells[ 8 ];    // Version 3: 1 nibble/cell // 8 rows of 4 bits/piece
@@ -17,7 +17,44 @@ struct PrettyPrintBoard_t
         memset( _cells, PIECE_EMPTY, sizeof( _cells ) );
     }
 
-    void Print( bool bPrintRankFile = true )
+    void PrintFile( int nSpacer = 1 )
+    {
+        printf( "  " );
+        for( int x = 0; x < 8; x++ )
+            printf( "%c%*s", aFILE[ x ], nSpacer, "" );
+        printf( "\n" );
+    }
+
+    void PrintCompact( bool bPrintRankFile = true )
+    {
+        char *p = _cells;
+
+        for( int iCell = 0; iCell < 64; iCell++, p++ )
+        {
+            char iCol    = iCell & 7;
+            char iRow    = iCell / 8;
+
+            int  iPiece  = *p & 0xF;
+            char cPiece  = aPIECES [ iPiece  ];
+            bool bOdd    = ((iCell + iRow) & 1);
+
+            if (bPrintRankFile && (iCol == 0))
+                printf( "%c ", aRANK[ 7 - iCell/8 ] );
+
+            if( (iPiece & 7) == PIECE_EMPTY )
+                printf( "%c", " ."[ bOdd ] );
+            else
+                printf( "%c", cPiece );
+
+            if (iCell && (iCol == 7))
+                printf( "\n" );
+        }
+
+        if ( bPrintRankFile )
+            PrintFile( 0 );
+    }
+
+    void PrintPretty( bool bPrintRankFile = true )
     {
         char *p = _cells;
 
@@ -45,13 +82,8 @@ struct PrettyPrintBoard_t
                 printf( "%s\n", CELL_EOL );
         }
 
-        if (bPrintRankFile )
-        {
-             printf( "  " );
-             for( int x = 0; x < 8; x++ )
-               printf( "%c ", aFILE[ x ] );
-            printf( "\n" );
-        }
+        if ( bPrintRankFile )
+            PrintFile();
     }
 };
 
@@ -77,7 +109,7 @@ struct StateBitBoard_t
         }
     }
 
-    void BitBoardToPrettyPrint( int iPlayer, PrettyPrintBoard_t *board_ )
+    void BitBoardToRaw( int iPlayer, Board_t *board_ )
     {
         for( int iPiece = PIECE_PAWN; iPiece < NUM_PIECES; iPiece++ )
         {
@@ -212,18 +244,29 @@ printf( "INFO: State   : %u bytes\n", (uint32_t) sizeof( *this    ) );
         }
     }
 
-    void PrettyPrintBoard( bool bPrintRankFile = true )
+    void _GetBoard( Board_t& board )
     {
-        PrettyPrintBoard_t board;
         board.Init();
 
         for( int iPlayer = 0; iPlayer < NUM_PLAYERS; iPlayer++ )
         {
             StateBitBoard_t *pState = &_player[ iPlayer ];
-            pState->BitBoardToPrettyPrint( iPlayer, &board );
+            pState->BitBoardToRaw( iPlayer, &board );
         }
+    }
 
-        board.Print( bPrintRankFile );
+    void CompactPrintBoard( bool bPrintRankFile = true )
+    {
+        Board_t board;
+        _GetBoard( board );
+        board.PrintCompact( bPrintRankFile );
+    }
+
+    void PrettyPrintBoard( bool bPrintRankFile = true )
+    {
+        Board_t board;
+        _GetBoard( board );
+        board.PrintPretty( bPrintRankFile );
     }
 
 inline uint8_t GetColorPlayer() { return  _bFlags &  STATE_WHICH_PLAYER; }
