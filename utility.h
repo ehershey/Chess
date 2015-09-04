181646cdb4095b5934afc18a03cd8cea38c7f6f0
Michaelangel007 <testcraft@sharedcraft.com>
2015-09-04 13:49:20 -0700

    Add symbolic Algebraic square names

--- utility.h
+++ utility.h
@@ -12,6 +12,17 @@
     +------------------------
       A1 B1 C1 D1 E1 F1 G1 H1
 */
+enum SquareNames_e
+{
+    _A8 = 0x70, _B8, _C8, _D8, _E8, _F8, _G8, _H8
+    _A7 = 0x60, _B7, _C7, _D7, _E7, _F7, _G7, _H7
+    _A6 = 0x50, _B6, _C6, _D6, _E6, _F6, _G6, _H6
+    _A5 = 0x40, _B5, _C5, _D5, _E5, _F5, _G5, _H5
+    _A4 = 0x30, _B4, _C4, _D4, _E4, _F4, _G4, _H4
+    _A3 = 0x20, _B3, _C3, _D3, _E3, _F3, _G3, _H3
+    _A2 = 0x10, _B2, _C2, _D2, _E2, _F2, _G2, _H2
+    _A1 = 0x00, _B1, _C1, _D1, _E1, _F1, _G1, _H1
+};
 
 // i -> x,y
 void CellToColRow( const uint8_t cell, uint8_t& col_, uint8_t& row_ )
