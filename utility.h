dad0d9bb39600d5306ad73aa5010798f90993fb1
michaelangel007 <testcraft@sharedcraft.com>
2015-09-05 13:44:16 -0700

    Add INVALID_MOVE_RF

--- utility.h
+++ utility.h
@@ -24,7 +24,8 @@ enum RankFileNames_e
     _A4 = 0x30, _B4, _C4, _D4, _E4, _F4, _G4, _H4,
     _A3 = 0x20, _B3, _C3, _D3, _E3, _F3, _G3, _H3,
     _A2 = 0x10, _B2, _C2, _D2, _E2, _F2, _G2, _H2,
-    _A1 = 0x00, _B1, _C1, _D1, _E1, _F1, _G1, _H1
+    _A1 = 0x00, _B1, _C1, _D1, _E1, _F1, _G1, _H1,
+    INVALID_MOVE_RF = -1
 };
 
 // i -> x,y
