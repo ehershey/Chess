13f1642e2aba2b1437333f46d8ce674e2c2acf12
michaelangel007 <testcraft@sharedcraft.com>
2015-09-05 13:44:53 -0700

    Cleanup state flags - keep track of current depth for searching

--- state.h
+++ state.h
@@ -168,20 +168,23 @@ enum MoveFlags_e
 struct State_t
 {
     StateBitBoard_t _player[ NUM_PLAYERS ]; // 96 bytes if not storing board[ PIECE_EMPTY ]
-    //                            //      96  112 bytes
+    // Move                     ; //      96  112 bytes
     uint8_t         _bFlags     ; // +1   97  113
     uint8_t         _bMoveType  ; // +1   98  114  4-bites: Piece move 4-bits: Type of move or capture
-    uint16_t        _iParent    ; // +2  100  116  i-node of parent
-    uint16_t        _iFirstChild; // +2  102  118  i-node of first child
-    uint8_t         _nChildren  ; // +1  103  119  total children
-    uint8_t         _nBestMoveRF; // +1  104  120
-     int16_t        _nEval      ; // +2  106  122
-    uint8_t         _iFrom      ; // +1  107  123  RankFile (0xROWCOL)
-    uint8_t         _iTo        ; // +1  108  124  RankFile (0xROWCOL)
-    uint8_t         _bPawnsMoved; // +1  109  125  8-bits: Bitflags if pawn[col] has moved
-    uint8_t         _iGamePhase ; // +1  110  126  0x00=early game, 0x80=mid-game, 0xFF=end-game
-    uint8_t         _pad1       ; // ============
-    uint8_t         _pad2       ; //     112  128
+    uint8_t         _iSrcRF     ; // +1   99  115  From RankFile (0xROWCOL)
+    uint8_t         _iDstRF     ; // +1  100  116  Unto RankFile (0xROWCOL)
+    uint8_t         _bPawnsMoved; // +1  101  117  8-bits: Bitflags if pawn[col] has moved
+    uint8_t         _iGamePhase ; // +1  102  118  0x00=early game, 0x80=mid-game, 0xFF=end-game
+    // Children                 ;
+    uint16_t        _iParent    ; // +2  104  120  i-node of parent
+    uint16_t        _iFirstChild; // +2  106  122  i-node of first child
+    uint8_t         _nChildren  ; // +1  107  123  total children
+    // Eval & Search            ;
+    uint8_t         _nDepth     ; // +1  108  124
+     int16_t        _nEval      ; // +2  110  126
+    uint8_t         _nBestMoveRF; // +1  111  127
+    // Unused                   ; // ============  Current depth when searching
+    uint8_t         _pad        ; //     112  128
 
     void Zero()
     {
@@ -204,15 +207,24 @@ struct State_t
 printf( "INFO: Boards[]: %u bytes\n", (uint32_t) sizeof( _player ) );
 printf( "INFO: State   : %u bytes\n", (uint32_t) sizeof( *this    ) );
 
-        _nEval  = 0;
-        _bFlags = 0 | STATE_CAN_CASTLE_Q_SIDE | STATE_CAN_CASTLE_K_SIDE;
-        _iFrom  = 0;
-        _iTo    = 0;
-//printf( "State.Flags: %08X\n", _bFlags );
+        // Move
+        _bFlags      = 0 | STATE_CAN_CASTLE_Q_SIDE | STATE_CAN_CASTLE_K_SIDE;
+        _bMoveType   = 0;
+        _iSrcRF      = 0;
+        _iDstRF      = 0;
         _bPawnsMoved = 0;
+        _iGamePhase  = 0;
+
+        // Children
         _iParent     = 0;
         _iFirstChild = 1;
         _nChildren   = 0;
+
+        // Eval & Search
+        _nDepth      = 0;
+        _nEval       = 0;
+        _nBestMoveRF = INVALID_MOVE_RF;
+//printf( "State.Flags: %08X\n", _bFlags );
 
         StateBitBoard_t *pState;
 
@@ -304,7 +316,7 @@ inline void    TogglePlayer  () {         _bFlags ^= STATE_WHICH_PLAYER; }
         return board;
     }
 
-    bool IsCheck( uint8_t nKingRF ) // = 0xFF )
+    bool IsCheck( uint8_t nKingRF ) // = INVALID_MOVE_RF
     {
         uint8_t iEnemy  = GetColorEnemy ();
 
