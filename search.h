bf7d5cdc84cbf9c52c1234c7d5a42b636199e72b
Michaelangel007 <testcraft@sharedcraft.com>
2015-09-10 09:51:53 -0700

    Add SearchMove and SearchState

--- search.h
+++ search.h
@@ -4,3 +4,22 @@ http://www.chessbin.com/post/Quiescence-Search-and-Extensions.aspx
 http://www.chessbin.com/post/Move-Searching-and-Alpha-Beta.aspx
 
 */
+
+const int MAX_SEARCH_MOVES = 63; // Don't need to search moving into our current position
+
+struct SearchMove_t
+{
+    uint8_t _nMoves;
+    uint8_t _aMoves[ 63 ]; // 0xRankFile
+};
+
+/* To search we need:
+      initial board layout
+      list of possible moves to search and evaluate thier strength
+*/
+struct SearchState_t
+{
+    State_t      _state;
+    SearchMove_t _moves;
+};
+
